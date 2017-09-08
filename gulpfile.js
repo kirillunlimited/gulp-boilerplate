d4c640c33d0b4c95c988cf4a1d01b0f995b19473
Kirill Ivanov <kirillunlimited@gmail.com>
2017-09-08 17:57:03 +0300

    Add JS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 
 var sass = require('gulp-sass');
 var csso = require('gulp-csso');
@@ -8,8 +9,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-  return gulp
-    .src('./demo/src/sass/index.scss')
+  return gulp.src('./demo/src/sass/index.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write({includeContent: false}))
@@ -18,6 +18,30 @@ gulp.task('sass', function () {
     .pipe(csso())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./demo/dist/css'));
+});
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jsValidate = require('gulp-jsvalidate');
+
+gulp.task('js', function() {
+  return gulp.src('./demo/src/js/**/*.js')
+    .pipe(jsValidate())
+    .on('error', function(error) {
+      gutil.log(gutil.colors.red('Error') + ' in JavaScript');
+      gutil.log('  Filename:');
+      gutil.log('      ' + error.fileName);
+      gutil.log('  Error:');
+      gutil.log('      ' + error.message );
+      this.emit('end');
+    })
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(sourcemaps.write({includeContent: false}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./demo/dist/js'))
 });
 
 var clean = require('gulp-clean');
@@ -29,8 +53,9 @@ gulp.task('clean', function () {
 
 gulp.task('watch', function(){
   gulp.watch('demo/src/sass/**/*.{sass,scss}', ['sass']);
+  gulp.watch('demo/src/js/**/*.js', ['js']);
 });
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'js']);
 
 gulp.task('default', ['build', 'watch']);
