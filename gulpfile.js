1aabe2bc286954a74c2deb128c3e0390d620bb31
Kirill Ivanov <kirillunlimited@gmail.com>
2017-09-08 17:09:59 +0300

    Add Sass task

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./demo/src/sass/index.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write({includeContent: false}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./demo/dist/css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('demo/src/sass/**/*.{sass,scss}', ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'watch']);
