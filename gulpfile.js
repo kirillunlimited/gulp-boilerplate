839a9f3425e610231f147cf8fc6efecffa8e99ec
Kirill Ivanov <kirillunlimited@gmail.com>
2017-11-13 21:57:41 +0300

    Add "gulp-load-plugins" to get rid of requires

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-
 const gulp = require('gulp');
-const gutil = require('gulp-util');
-const sourcemaps = require('gulp-sourcemaps');
-const runSequence  = require('run-sequence');
+const $ = require('gulp-load-plugins')({
+  pattern: '*'
+});
 
 const CONFIG = {
   DEST: "./demo/dist",
@@ -38,72 +37,55 @@ const CONFIG = {
   },
 };
 
-const sass = require('gulp-sass');
-const csso = require('gulp-csso');
-const autoprefixer = require('gulp-autoprefixer');
-
 gulp.task('sass', function () {
   return gulp.src(CONFIG.SASS.SRC_FILE)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write({includeContent: false}))
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(autoprefixer())
-    .pipe(csso())
-    .pipe(sourcemaps.write())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.sourcemaps.write({includeContent: false}))
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.autoprefixer())
+    .pipe($.csso())
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest(CONFIG.SASS.DEST));
 });
 
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const jsValidate = require('gulp-jsvalidate');
-
 gulp.task('js', function() {
   return gulp.src(CONFIG.JS.SRC)
-    .pipe(jsValidate())
+    .pipe($.jsvalidate())
     .on('error', function(error) {
-      gutil.log(gutil.colors.red('Error') + ' in JavaScript');
-      gutil.log('  Filename:');
-      gutil.log('      ' + error.fileName);
-      gutil.log('  Error:');
-      gutil.log('      ' + error.message );
+      $.util.log($.util.colors.red('JavaScript error'));
+      $.util.log('  ' + $.util.colors.yellow('Filename: ') + error.fileName);
+      $.util.log('  ' + $.util.colors.yellow('Error:    ') + error.message);
       this.emit('end');
     })
-    .pipe(sourcemaps.init())
-    .pipe(concat(CONFIG.JS.DEST_FILE))
-    .pipe(sourcemaps.write({includeContent: false}))
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
+    .pipe($.sourcemaps.init())
+    .pipe($.concat(CONFIG.JS.DEST_FILE))
+    .pipe($.sourcemaps.write({includeContent: false}))
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.uglify())
+    .pipe($.sourcemaps.write())
     .pipe(gulp.dest(CONFIG.JS.DEST));
 });
 
-const imagemin = require('gulp-imagemin');
-const changed = require('gulp-changed');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
-
 gulp.task('images', function() {
   return gulp.src(CONFIG.IMAGES.SRC)
-    .pipe(changed(CONFIG.IMAGES.DEST))
-    .pipe(imagemin([
-      imageminPngquant({quality: '50-75'}),
-      imageminJpegRecompress({
+    .pipe($.changed(CONFIG.IMAGES.DEST))
+    .pipe($.imagemin([
+      $.imageminPngquant({quality: '50-75'}),
+      $.imageminJpegRecompress({
         progressive: true,
         max: 75,
         min: 50,
         quality: 'medium'
       }),
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.svgo({plugins: [{removeViewBox: true}]})
+      $.imagemin.gifsicle({interlaced: true}),
+      $.imagemin.svgo({plugins: [{removeViewBox: true}]})
     ]))
     .pipe(gulp.dest(CONFIG.IMAGES.DEST));
 });
 
-const spritesmith = require('gulp.spritesmith');
-
 gulp.task('sprite', function() {
-  const spriteData = gulp.src(CONFIG.SPRITE.SRC).pipe(spritesmith({
+  const spriteData = gulp.src(CONFIG.SPRITE.SRC).pipe($.spritesmith({
     imgName: CONFIG.SPRITE.NAME,
     cssName: CONFIG.SPRITE.NAME_CSS,
     imgPath: CONFIG.SPRITE.PATH
@@ -118,9 +100,8 @@ gulp.task('sprite', function() {
   return spriteData;
 });
 
-const browserSync = require('browser-sync').create();
-
 gulp.task('serve', ['default'], function() {
+  const browserSync = $.browserSync.create();
   browserSync.init({
     server: {
       baseDir: CONFIG.BROWSERSYNC.BASE
@@ -130,6 +111,7 @@ gulp.task('serve', ['default'], function() {
 });
 
 gulp.task('serve:proxy', ['default'], function() {
+  const browserSync = $.browserSync.create();
   browserSync.init({
     startPath: CONFIG.BROWSERSYNC.BASE,
     proxy: {
@@ -139,11 +121,9 @@ gulp.task('serve:proxy', ['default'], function() {
   gulp.watch(CONFIG.BROWSERSYNC.DEST).on('change', browserSync.reload);
 });
 
-const clean = require('gulp-clean');
-
 gulp.task('clean', function () {
   return gulp.src(CONFIG.DEST, {read: false})
-    .pipe(clean({force: true}));
+    .pipe($.clean({force: true}));
 });
 
 gulp.task('watch', function(){
@@ -154,7 +134,7 @@ gulp.task('watch', function(){
 });
 
 gulp.task('build', function() {
-  runSequence('sprite', 'sass', 'js', 'images');
+  $.runSequence('sprite', 'sass', 'js', 'images');
 });
 
 gulp.task('default', ['build', 'watch']);
